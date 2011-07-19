15b2d2e732d355298da0095231e483e37f292191
Dean Wilson <dean.wilson@gmail.com>
2011-07-19 14:47:58 +0100

    Add yum-postprocessor

---
+++ postprocessor.py
@@ -0,0 +1,57 @@
+from yum.plugins import PluginYumExit, TYPE_CORE, TYPE_INTERACTIVE
+import subprocess
+
+requires_api_version = '2.5'
+
+plugin_type = (TYPE_INTERACTIVE,)
+
+def config_hook(conduit):
+    parser = conduit.getOptParser()
+    parser.add_option('', '--post-process', dest='postprocess', action='store_true',
+           default=True, help="show pending package changes as JSON")
+
+
+def posttrans_hook(conduit):
+    opts, commands = conduit.getCmdLine()
+
+    command = conduit.confString("main", "command", default=None)
+
+    if opts.postprocess:
+        action   = commands[0]
+        packages = {}
+
+        for transaction in conduit.getTsInfo():
+            if transaction.name not in packages:
+                packages[transaction.name] = {}
+
+
+            packages[transaction.name]["action"] = action
+
+            if action == "downgrade":
+                if transaction.ts_state is "e":
+                    packages[transaction.name]["current"] = transaction.version
+
+                if transaction.ts_state is "u":
+                    packages[transaction.name]["pending"] = transaction.version
+
+            elif action == "update":
+                if transaction.ts_state is "u":
+                    packages[transaction.name]["pending"] = transaction.version
+
+                if transaction.ts_state is None:
+                    packages[transaction.name]["current"] = transaction.version
+
+            else:
+                packages[transaction.name]["pending"] = transaction.version
+
+
+        for package in packages:
+            if action in [ "install", "remove" ]:
+                details = "action=%s name=%s version=%s" % ( packages[package]["action"], package, packages[package]["pending"] )
+                subprocess.call([ command, details])
+
+
+            if action in [ "update", "downgrade" ]:
+                details = "action=%s name=%s version=%s pending=%s" % \
+                     ( packages[package]["action"], package, packages[package]["current"], packages[package]["pending"] )
+                subprocess.call([ command, details])
